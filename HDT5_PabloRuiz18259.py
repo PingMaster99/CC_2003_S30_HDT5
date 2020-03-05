42feb18472f5ee31265aa1043320d443fdca2bd9
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-03-04 19:31:46 -0600

    Finished program and comments

--- HDT5_PabloRuiz18259.py
+++ HDT5_PabloRuiz18259.py
@@ -1,51 +1,83 @@
 import simpy
 import random
+import statistics
 
 random.seed(1234567890)
-numberOfProcesses = 25  #Defines the amount of processes to be executed
-CPUprocesses = 3
+timeValues = []
+numberOfProcesses = 25    # Defines the amount of processes to be executed
+CPUCores = 1              # Number of processors
+CPUThreads = 3            # Number of instructions executed by each processor
+RAMCapacity = 100         # RAM size
+instructionInterval = 10  # Speed at which processes are received, higher is faster (cannot be zero)
 
-def proceso(name, env, processTime, cpu, wait):
 
-    # Waits for a RAM assignation
-    yield env.timeout(processTime)
+def proceso(name, env, cpu, wait, RAM):
+    timeReceived = env.now    # Time the instruction is received
+    RAMQuantity = random.randint(1, 10)    # RAM required
+    instructionNumber = random.randint(1, 10)    # Number of steps in the instruction
 
-    timeReceived = env.now
-    RAMQuantity = random.randint(1, 10)
-    instructionNumber = random.randint(1, 10)
-    print('%s llega a las %f necesita %d de memoria RAM y cuenta con %d instrucciones' %
+    print('%s arrives at %f, needs %d of RAM, and has %d instructions' %
           (name, timeReceived, RAMQuantity, instructionNumber))
 
-    if(RAM.level >= RAMQuantity):
-        RAM.get(RAMQuantity)
-        print("%s READY" % name)
-        print("Quedan %i de RAM" % RAM.level)
+    # Allocates ram to the instruction if available
+    with RAM.get(RAMQuantity) as allocatedRAM:
+        yield allocatedRAM
+        yield env.timeout(1)
+        print("%s READY" % name)    # Instruction is ready once RAM is allocated
 
+        # Instruction is in CPU while it still has instructions left
+        while instructionNumber > 0:
+            print('%s RUNNING' % name)    # CPU runs the instruction
+            # Instruction is assigned a turn in the CPU
+            with cpu.request() as turn:
+                yield turn
+                # If the instruction count is less than the number of threads, the processor is released sooner
+                if instructionNumber <= CPUThreads:
+                    yield env.timeout(1)
+                    instructionNumber = 0   # Instruction is finished
+                # If the instruction has more than 3 instructions, it processes only 3 of them
+                else:
+                    yield env.timeout(1)
+                    instructionNumber -= CPUThreads
+                    # Checks to see if it needs to perform IO operations
+                    if random.randint(1, 2) == 1:
+                        # Performs the IO operations
+                        with wait.request() as operation:
+                            yield operation
+                            print("%s WAITING" % name)
+                            yield env.timeout(1)
 
+        # Returns the amount of RAM allocated
+        with RAM.put(RAMQuantity) as returnRAM:
+            yield env.timeout(1)
+            yield returnRAM
 
-        with cpu.request() as turn:
-            yield turn
-            yield env.timeout(instructionNumber)
-            print('%s sale a las %f del CPU' % (name, env.now))
-
-    else:
-        print("No hay suficiente RAM disponible, instruccion en cola")
-
+    # Records the process time
     totalTime = env.now - timeReceived
-    print('%s se tardo %f' % (name, totalTime))
+    print('%s TERMINATED in %f' % (name, totalTime))
+    timeValues.append(totalTime)
     print()
 
 
-
-
+# Conditions for the simulating environment
 env = simpy.Environment()   # Simulation environment
-RAM = simpy.Container(env, init=100, capacity=100)
-CPU = simpy.Resource(env, capacity=1)
+RAM = simpy.Container(env, init=RAMCapacity, capacity=RAMCapacity)
+CPU = simpy.Resource(env, capacity=CPUCores)
 WAITING = simpy.Resource(env, capacity=1)   # Queue of I/O
 
 
-for i in range(numberOfProcesses):
-    env.process(proceso("Instruccion %d" % i, env, random.expovariate(1.0 / 10), CPU, WAITING))
+# Runs the processes
+def main(environment, ram, cpu, waiting):
+    for i in range(numberOfProcesses):
+        env.process(proceso("PROCESS %d" % i, environment, cpu, waiting, ram))
+        yield env.timeout(random.expovariate(1.0 / instructionInterval))
 
 
-env.run(until=100)
+# Runs the environment
+env.process(main(env, RAM, CPU, WAITING))
+env.run()
+
+# Prints the needed statistics from the process times
+print("The average process time is %f" % statistics.mean(timeValues))
+print("Its standard deviation is %f" % statistics.stdev(timeValues))
+
